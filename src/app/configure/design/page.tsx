db5b42a2ab5af7a2b73081bb873b52d1b4f8e31c
ousmaneDiakhaby <diakhaby.ousmane@outlook.com>
2024-12-13 17:08:35 +0100

    chore: initial commit

---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,9 @@
+const Page = async () => {
+    // appel à la base de données
+
+    return (
+        <p></p>
+    )
+}
+
+export default Page
---
+++ src/app/configure/layout.tsx
@@ -0,0 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import {ReactNode} from "react";
+import Steps from "@/components/Steps";
+
+const Layout = ({children}:{ children: ReactNode }) => {
+    return (
+        <MaxWidthWrapper className='flex-1 flex flex-col'>
+            <Steps/>
+            {children}
+        </MaxWidthWrapper>
+    )
+}
+
+export default Layout
